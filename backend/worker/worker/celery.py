4196df6347f6b5f61f16231c6473922680c29a8d
shrewd-laidback palace <shrewd-laidback-palace-736-c41-2c1-e464fc974@swiss-armed-forces-open-source.ch>
2025-07-05 11:37:37 +0000

    Reduce load from celery worker ping
    The using the -A option for celery worker ping is not ideal
    and creates a lot of load, database connections and potentially
    defunced processes.
    
    See merge request swiss-armed-forces/cyber-command/cea/loom!91

--- backend/worker/worker/celery.py
+++ backend/worker/worker/celery.py
@@ -1,13 +1,25 @@
 """Provides the worker entry point."""
 
 import logging
+import sys
 
+from celery import Celery
+from common.celery_app import init_minimal_celery_app
 from common.dependencies import get_celery_app
 from common.dependencies import init as init_common_dependencies
 
 from worker.dependencies import init
 
 logger = logging.getLogger(__name__)
-init_common_dependencies()
-init()
-app = get_celery_app()
+
+app: Celery
+# Return a minimal celery app if we are running the inspect command.
+# This is to facilitate lightweight running of inspect command.
+if "inspect" in sys.argv:
+    logger.info("Initializing minimal Celery app")
+    app = init_minimal_celery_app()
+else:
+    logger.info("Initializing Celery app")
+    init_common_dependencies()
+    init()
+    app = get_celery_app()
