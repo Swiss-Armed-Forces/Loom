b1ec78c2de405ba6414cff42ff73466340f774d8
shrewd-laidback palace <shrewd-laidback-palace-736-c41-2c1-e464fc974@swiss-armed-forces-open-source.ch>
2025-09-29 16:58:50 +0000

    Resolve "Minio Crawler stops crawling when loosing connection to mongodb"
    # 📌 Summary
    
    ## 🔗 Issue Reference
    
    
    Closes #157
    
    See merge request swiss-armed-forces/cyber-command/cea/loom!177

--- backend/crawler/crawler/minio_crawler.py
+++ backend/crawler/crawler/minio_crawler.py
@@ -5,6 +5,7 @@ from threading import Thread
 
 from common.dependencies import get_file_scheduling_service, get_lazybytes_service
 from minio import Minio
+from minio.datatypes import Bucket
 
 from crawler.settings import settings
 
@@ -17,64 +18,76 @@ MINIO_READ_CHUNK_SIZE: int = 64 * 1024
 class MinioCrawler:
     def __init__(self, minio_client: Minio):
         self.client = minio_client
+        self.bucket_crawlers: dict[Bucket, Thread] = {}
 
-        self.buckets: set[str] = set()
-        self.collect_all_buckets()
+    def _reap_dead_bucket_crawlers(self):
+        # Collect buckets to remove first
+        buckets_to_remove = []
+        for bucket, crawler in self.bucket_crawlers.items():
+            if not crawler.is_alive():
+                logger.info("Reaping crawler for bucket: %s", bucket)
+                buckets_to_remove.append(bucket)
+
+        # Then remove them
+        for bucket in buckets_to_remove:
+            del self.bucket_crawlers[bucket]
+
+    def _create_bucket_crawler_for_new_buckets(self):
+        for bucket in self.client.list_buckets():
+            if bucket not in self.bucket_crawlers:
+                logger.info("New bucket detected: %s", bucket)
+                self._create_bucket_crawler_thread(bucket)
+
+    def _create_bucket_crawler_thread(self, bucket: Bucket):
+        bucket_crawler_thread = _BucketCrawlerThread(self.client, bucket)
+        bucket_crawler_thread.start()
+        self.bucket_crawlers[bucket] = bucket_crawler_thread
 
     def crawl(self):
         while True:
-            buckets = self.client.list_buckets()
-            for bucket in buckets:
-                if bucket.name not in self.buckets:
-                    logger.info("New bucket detected %s", bucket.name)
-                    self.buckets.add(bucket.name)
-                    self.download_all_files(bucket.name)
-                    self.create_bucket_notification_thread(bucket.name)
-
+            self._reap_dead_bucket_crawlers()
+            self._create_bucket_crawler_for_new_buckets()
             time.sleep(MINIO_BUCKET_CHECK_INTERVAL_S)
 
-    def collect_all_buckets(self):
-        buckets = self.client.list_buckets()
-        for bucket in buckets:
-            self.buckets.add(bucket.name)
-            self.download_all_files(bucket.name)
-            self.create_bucket_notification_thread(bucket.name)
 
-    def download_file(self, bucket_name: str, file_name: str):
-        logger.info("Crawler detected file %s", file_name)
-        full_name = Path(f"//{bucket_name}/{file_name}")
-        source = f"{settings.crawler_source_id}/{bucket_name}"
+class _BucketCrawlerThread(Thread):
+
+    def __init__(self, client: Minio, bucket: Bucket):
+        super().__init__()
+        self.client = client
+        self.bucket = bucket
+
+    def run(self):
+        self._download_all_files()
+        self._listen_for_bucket_notification()
+
+    def _download_file(self, file_name: str):
+        logger.info("Crawler downloading file %s", file_name)
+        full_name = Path(f"//{self.bucket.name}/{file_name}")
+        source = f"{settings.crawler_source_id}/{self.bucket.name}"
         get_file_scheduling_service().index_file(
             str(full_name),
             get_lazybytes_service().from_generator(
-                self.client.get_object(bucket_name, file_name).stream(
+                self.client.get_object(self.bucket.name, file_name).stream(
                     MINIO_READ_CHUNK_SIZE
                 )  # type: ignore
             ),
             source,
         )
 
-    def download_all_files(self, bucket_name: str):
-        files = self.client.list_objects(bucket_name, recursive=True)
+    def _download_all_files(self):
+        files = self.client.list_objects(self.bucket.name, recursive=True)
         for file in files:
             if file.object_name is not None:
-                self.download_file(bucket_name, file.object_name)
+                self._download_file(file.object_name)
 
-    def create_bucket_notification_thread(self, bucket_name: str):
-        notification_thread = Thread(
-            target=_listen_for_notification, args=(self, bucket_name), daemon=True
-        )
-
-        notification_thread.start()
-
-
-def _listen_for_notification(minio_crawler: MinioCrawler, bucket_name: str):
-    with minio_crawler.client.listen_bucket_notification(
-        bucket_name,
-        prefix="",
-        events=["s3:ObjectCreated:*"],  # type: ignore
-    ) as events:
-        for event in events:
-            for record in event.get("Records", []):
-                file_name = record["s3"]["object"]["key"]
-                minio_crawler.download_file(bucket_name, file_name)
+    def _listen_for_bucket_notification(self):
+        with self.client.listen_bucket_notification(
+            self.bucket.name,
+            prefix="",
+            events=["s3:ObjectCreated:*"],  # type: ignore
+        ) as events:
+            for event in events:
+                for record in event.get("Records", []):
+                    file_name = record["s3"]["object"]["key"]
+                    self._download_file(file_name)
