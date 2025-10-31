b7afe63af5389bbfbae1dbd5f93bb5bb4a9b292d
shrewd-laidback palace <shrewd-laidback-palace-736-c41-2c1-e464fc974@swiss-armed-forces-open-source.ch>
2025-10-31 13:28:27 +0000

    test_dead_letter_tasks: Remove tests router and associated functionality
    # 📌 Summary
    
    ## 🔗 Issue Reference
    
    See merge request swiss-armed-forces/cyber-command/cea/loom!204
    
    Co-authored-by: open-source Pipeline <group_90701827_bot_ed04ae348bc5f40af9966fb8b6867e99@noreply.gitlab.com>

--- integrationtest/tests/dead_letter_queue_test.py
+++ integrationtest/tests/dead_letter_queue_test.py
@@ -1,30 +1,16 @@
 import logging
-from time import sleep
 
 import pytest
-import requests
-
-from utils.celery_inspect import is_celery_idle
-from utils.consts import REQUEST_TIMEOUT, TESTS_ENDPOINT
+from common.celery_app import DeadTask
+from common.dependencies import get_celery_app
 
 logger = logging.getLogger(__name__)
 
+GET_TIMEOUT = 180
 
-@pytest.mark.timeout(30)
+
 def test_dead_letter_tasks():
-    response = requests.post(
-        f"{TESTS_ENDPOINT}/dispatch_sigkill_pgroup_task", timeout=REQUEST_TIMEOUT
-    )
-    response.raise_for_status()
-
-    # This polling-wait does not work in most cases
-    # as the task scheduled form the previous api call
-    # does not cause the queue/celery to be busy.
-    #
-    # Same issues as:
-    #  - https://gitlab.com/swiss-armed-forces/cyber-command/cea/loom/-/issues/25
-    #
-    # But we keep this test here anyways - logically the test is correct.
-    while not is_celery_idle():
-        logger.debug("Celery not idle yet")
-        sleep(0.1)
+    with pytest.raises(DeadTask):
+        get_celery_app().send_task(
+            "worker.test.sigkill_pgroup_task.sigkill_pgroup_task"
+        ).get(timeout=GET_TIMEOUT)
