00f01e851e346d69ea39245f005b7309a5b984a6
zealous-alert zodiac <zealous-alert-zodiac-622-9c7-ca2-a7bc29c12@swiss-armed-forces-open-source.ch>
2026-03-16 18:37:56 +0000

    Fix slow animations by setting transitions none on global theme
    # :pushpin: Summary
    
    Disable global Material UI transitions by overriding `theme.transitions.create` to return `"none"`.\
    This makes the UI feel more responsive and snappier by removing animation delays across components.
    
    See merge request swiss-armed-forces/cyber-command/cea/loom!385

--- Frontend/src/app/theme.ts
+++ Frontend/src/app/theme.ts
@@ -1,6 +1,9 @@
 import { createTheme } from "@mui/material";
 
 export const globalTheme = createTheme({
+    transitions: {
+        create: () => "none",
+    },
     components: {
         MuiButton: {
             styleOverrides: {
